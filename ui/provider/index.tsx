65c3f100f504228d36cc8d19452a6cf7cffc50c1
Aloy <gate.2017.aloy@gmail.com>
2026-05-21 23:24:48 +0530

    setup providers

---
+++ ui/provider/index.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export const AppProvider = ({ children }: { children: ReactNode }) => {
+    const [queryClient] = useState(() => new QueryClient())
+
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </ThemeProvider>
+    )
+}
